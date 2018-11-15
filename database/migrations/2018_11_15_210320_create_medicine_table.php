0c004503d4cb843a44701ae07100ff41fc53c807
Ermin Šabotić <ermin@misijaweb.com>
2018-11-15 22:55:08 +0100

    initial push, project start

---
+++ database/migrations/2018_11_15_210320_create_medicine_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMedicineTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medicine', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->string('name');
+            $table->string('slug');
+            $table->integer('quantity')->default(0);
+            $table->tinyInteger('is_published')->default(0);
+
+            $table->unsignedInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medicine');
+    }
+}
